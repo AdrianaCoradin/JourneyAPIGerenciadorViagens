3d4818aab2642e268bb6547035a28f1e607fcaf1
Adriana <adrianacoradin44@gmail.com>
2024-07-11 22:17:52 -0300

    #000001

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        public IActionResult Register()
+        {
+            return Created();
+        }
+    }
+}
